1d925f27a603f9bfbb5544b1df7be266cfec8c49
Erwan BERNARD <bernard.erwan@gmail.com>
2019-06-19 15:35:26 +0200

    fix generator

--- src/golfbot/golfbot.cpp
+++ src/golfbot/golfbot.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-int main(){for(int x=53,i=1;i<1001;x=x*x%5651*5623,++i){std::cout<<(x%2?"╱":"╲")<<(i%100?"":"\n");}}
+int main(){for(int x=53,i=1;i<1001;x=x*x%(5651*5623),++i){std::cout<<(x%2?"╱":"╲")<<(i%100?"":"\n");}}
