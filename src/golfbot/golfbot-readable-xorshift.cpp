97cc6b8ef3bbc61922d1c27aebdfae80fc586513
Erwan BERNARD <bernard.erwan@gmail.com>
2019-06-20 09:32:01 +0200

    add optimisation on golfbot

--- src/golfbot/golfbot-readable-xorshift.cpp
+++ src/golfbot/golfbot-readable-xorshift.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 // Xorshift32
+// Golfed : int main(){for(int x=1,i=1;i<1001;[&]{x^=x<<13;x^=x>>17;x^=x<<5;}(),++i)std::cout<<(x%2?"╱":"╲")<<(i%100?"":"\n");}
 int main(int argc, char const *argv[]) {
   std::cout << "Noops Challenge 2019 - Golfbot" << std::endl;
   // ╱ : U+2571
   // ╲ : U+2572
 
-  long x = 1;  // seed
-  for (int j = 0; j < 20; ++j) {
-    for (int i = 0; i < 200; ++i) {
+  int x = 1;  // seed
+  for (int j = 0; j < 10; ++j) {
+    for (int i = 0; i < 100; ++i) {
       x ^= x<<13;
       x ^= x>>17;
       x ^= x<<5;
